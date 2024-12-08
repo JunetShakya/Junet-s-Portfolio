29b32db9e5d973d19351ea2b1aebf3cdaaf87322
Junet Shakya <shakyajunet.com>
2024-12-08 20:24:53 +0545

    Resolved merge conflict in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-=======
-  base:"./",
->>>>>>> 43f7032e93b4a728740f50ffe4c5e915dd433fbe
+  base: "./",
   build: {
     outDir: "./build",
   },
